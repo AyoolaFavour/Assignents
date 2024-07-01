8d9b0b03db126dea7b0ff3f5baebb73be56d93d6
AYOOLA FAVOUR <ayoolasamuel101@gmail.com>
2024-07-01 10:00:02 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-app';
+  Description ="A question on interpolation";
+  date = "20/02/20";
+
+  //an array of obejects called students
+  students=[
+    {id:1, name:'Ademola', age:"20"},
+    {id:2, name:'Great',   age:"18"},
+    {id:3, name:'Anisa',   age:"22"},
+    {id:4, name:'chinaza', age:"19"},
+    {id:5, name:'Patrick', age:"17"},
+    {id:6, name:'Desmond', age:"20"},
+    {id:7, name:'Barney',  age:"21"}
+  ];
+
+
+
+
 }
